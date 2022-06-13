8f722cd56f3f3bfecc14b03b89b1858308887503
alexissorianooo <alexis.soriano27@gmail.com>
2022-06-13 13:31:14 +0800

    design edit for search button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <div className='w-3/5 relative flex flex-row'>
           <input placeholder='Search notes...' className='bars bars-texts w-full px-5'/>
           <button 
-            className='bars bars-texts min-w-[10%] absolute right-0 bg-[#E8E8E8]'
+            className='bars bars-texts min-w-[10%] w-1/5 lg:w-[10%] absolute right-0 bg-[#E8E8E8]'
             
           >
             <i class="fa-solid fa-magnifying-glass"></i>
@@ -20,8 +20,10 @@ function App() {
           Add notes
         </button>
       </div>
-      {/* End for div of Search and Add notes */}
+      {/* End of div for Search and Add notes */}
+      {/* Div for Notes */}
       <div className=''>Notes should be here</div>
+      {/* End of div for Notes */}
     </>
   );
 }
