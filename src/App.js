d58081bd9764ebfd9d70ba6a01375d76b224447e
alexissorianooo <alexis.soriano27@gmail.com>
2022-06-14 13:47:20 +0800

    save button to local storage

--- src/App.js
+++ src/App.js
@@ -2,23 +2,60 @@ import React from 'react'
 import Modal from './components/Modal';
 
 function App() {
-  const [showModal, setShowModal] = React.useState(true)
+  const [showModal, setShowModal] = React.useState(false)
   const [note, setNote] = React.useState({
+    noteID: 0,
     noteTitle: '',
     noteContent: ''
   })
 
   function handleAddButton(){
     setShowModal(prevState => !prevState)
+    if(!showModal){
+      setNote(prevState => {
+        return{
+          ...prevState,
+          noteID: prevState.noteID+=1
+        }
+      })
+    }
+  }
+
+  function resetNote(){
+    setNote(prevState => {
+      return{
+        ...prevState,
+        noteTitle: '',
+        noteContent: ''
+      }
+    })
   }
 
   function handleSaveButton(){
-    
+    localStorage.setItem("note"+note.noteID, JSON.stringify(note))
+    resetNote()
+    setShowModal(prevState => !prevState)
   }
+
+  function handleInput(event){
+    let {name, value} = event.target
+    setNote(prevState => {
+      return{
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+
+  console.log(note)
 
   return (
     <div className='h-screen w-screen'>
-      {showModal && <Modal handleButton={handleAddButton}/>}
+      {showModal && <Modal 
+        handleButton={handleAddButton}
+        handleChange={(event) => handleInput(event)}
+        handleSave={handleSaveButton}
+        />}
       {/* Div for Search and Add notes */}
       <div className="flex flex-row w-screen justify-center items-center md:pt-[62px] pt-[31px]">
         <div className='w-3/5 relative flex flex-row'>
@@ -27,7 +64,7 @@ function App() {
             className='bars bars-texts min-w-[10%] w-1/5 lg:w-[10%] absolute right-0 bg-[#E8E8E8]'
             
           >
-            <i class="fa-solid fa-magnifying-glass"></i>
+            <i className="fa-solid fa-magnifying-glass"></i>
           </button>
         </div>
         <button 
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,12 +7,27 @@ export default function Modal(props){
             </div>
             <div className='h-5/6 w-5/6 bg-white rounded-2xl flex flex-col justify-between z-40 fixed right-0 left-0 top-0 bottom-0 m-auto '>
                 <div className='h-1/5 flex flex-col justify-center'>
-                    <input placeholder='Note title...' className='font-bold text-2xl sm:text-4xl lg:text-6xl p-5 w-4/5 focus:outline-0'/>
+                    <input 
+                        placeholder='Note title...' 
+                        className='font-bold text-2xl sm:text-4xl lg:text-6xl p-5 w-4/5 focus:outline-0'
+                        name="noteTitle"
+                        onChange={props.handleChange}
+                    />
                     <hr />
                 </div>
-                <textarea className=' h-3/5 p-8 lg:p-20 text-lg focus:outline-0 focus:bg-slate-100'/>
+                <textarea 
+                    placeholder='Enter notes here...'
+                    className=' h-3/5 p-8 lg:p-20 text-lg focus:outline-0 focus:bg-slate-100'
+                    name="noteContent"
+                    onChange={props.handleChange}
+                />
                 <div className='h-1/5 flex justify-end items-center'>
-                    <button className='button bars-texts bg-green-400 mr-5 p-2'>Save Note</button>
+                    <button 
+                        className='button bars-texts bg-green-400 mr-5 p-2'
+                        onClick={props.handleSave}
+                    >
+                        Save Note
+                    </button>
                 </div>
             </div>
         </>
