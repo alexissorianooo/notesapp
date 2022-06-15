ce8925f09c36cd5f49c4e6a5f3099a1b983a5c33
alexissorianooo <alexis.soriano27@gmail.com>
2022-06-15 16:46:47 +0800

    hover effect for notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -5,7 +5,7 @@ export default function Notes(props){
     let favorite = JSON.parse(localStorage.getItem("note"+props.noteID)).noteFavorite
 
     return(
-        <div className='bg-white w-72 min-h-72 my-2 sm:h-full sm:justify-self-center'>
+        <div className='bg-white w-72 min-h-72 my-2 sm:h-full sm:justify-self-center drop-shadow-sm hover:drop-shadow-lg'>
             <div className='min-h-1/5 p-4 font-bold text-lg flex '>
                 <div className='w-4/5'>{props.noteTitle}</div>
                 <br/>
